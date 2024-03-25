a541c7593a830eecf482ad650c6f3ef05d259905
VeljkoStefanovic613-2019 <85968664+VeljkoStefanovic613-2019@users.noreply.github.com>
2024-03-25 20:18:46 +0100

    Scroll To Top

--- src/components/Other/ScrollToTop.js
+++ src/components/Other/ScrollToTop.js
@@ -1,5 +1,11 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
 export const ScrollToTop = () => {
-  return (
-    <div>ScrollToTop</div>
-  )
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  },[pathname])
+
+  return null;
 }
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export { Footer } from "./Layout/Footer";
 
 export { ProductCard } from "./Elements/ProductCard";
 export { Rating } from "./Elements/Rating";
+
+export { ScrollToTop } from "./Other/ScrollToTop";
