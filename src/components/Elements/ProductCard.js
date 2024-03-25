febe3b460f2bfb848ce344e423b2a56d7d7bc1fb
VeljkoStefanovic613-2019 <85968664+VeljkoStefanovic613-2019@users.noreply.github.com>
2024-03-25 19:29:58 +0100

    Individual Product Card

--- src/components/Elements/ProductCard.js
+++ src/components/Elements/ProductCard.js
@@ -6,12 +6,12 @@ export const ProductCard = ({product}) => {
 
     return (
       <div className="m-3 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-          <Link to={`products/${id}`} className="relative" >
+          <Link to={`/products/${id}`} className="relative" >
               {best_seller && <span className="absolute top-4 left-2 px-2 bg-orange-500 bg-opacity-90 text-white rounded">Best Seller</span> }
               <img className="rounded-t-lg w-full h-64" src={poster} alt={name} />
           </Link>
           <div className="p-5">
-              <Link to={`products/${id}`}>
+              <Link to={`/products/${id}`}>
                   <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
               </Link>
               <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{overview}</p>
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,5 @@
 export { Header } from "./Layout/Header";
 export { Footer } from "./Layout/Footer";
+
+export { ProductCard } from "./Elements/ProductCard";
+export { Rating } from "./Elements/Rating";
