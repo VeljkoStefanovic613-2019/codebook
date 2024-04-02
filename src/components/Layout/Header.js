9f407854d7988b0425ad919d90c9595240a5e7f3
VeljkoStefanovic613-2019 <85968664+VeljkoStefanovic613-2019@users.noreply.github.com>
2024-04-02 15:56:11 +0200

    Dropdown Option

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 import Logo from "../../assets/logo.png";
 import { useEffect, useState } from "react";
 import { Search } from "../Sections/Search";
+import { DropdownLoggedOut, DropdownLoggedIn } from "../index";
 
 export const Header = () => {
     const [darkMode, setDarkMode] = useState(JSON.parse(localStorage.getItem("darkMode")) || false);
     const [searchShow, setSearchShow] = useState(false);
-    
+    const [dropdown, setDropdown] = useState (false);
+
     useEffect(() => {
         localStorage.setItem("darkMode", JSON.stringify(darkMode));
         if (darkMode){
@@ -33,11 +35,13 @@ export const Header = () => {
                         <span className="text-white text-sm absolute -top-1 left-2.5 bg-rose-500 px-1 rounded-full ">0</span>
                     </span>                    
                     </Link>
-                    <span className="bi bi-person-circle cursor-pointer text-2xl text-gray-700 dark:text-white"></span>
+                    <span onClick={() => setDropdown(!dropdown)} className="bi bi-person-circle cursor-pointer text-2xl text-gray-700 dark:text-white"></span>
+                    {dropdown && <DropdownLoggedOut />}
                 </div>
             </div>
         </nav>
         { searchShow && <Search setSearchShow={setSearchShow} /> }
+        
     </header>
   )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,8 @@ export { Footer } from "./Layout/Footer";
 export { ProductCard } from "./Elements/ProductCard";
 export { Rating } from "./Elements/Rating";
 
+export { DropdownLoggedIn } from "./Elements/DropdownLoggedIn";
+export { DropdownLoggedOut } from "./Elements/DropdownLoggedOut";
+
+
 export { ScrollToTop } from "./Other/ScrollToTop";
