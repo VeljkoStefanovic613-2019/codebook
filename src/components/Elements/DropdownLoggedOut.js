1c7a54a8aaa1083978b26b8b156c463383a9f3ad
VeljkoStefanovic613-2019 <85968664+VeljkoStefanovic613-2019@users.noreply.github.com>
2024-04-08 21:02:33 +0200

    Session Storage

--- src/components/Elements/DropdownLoggedOut.js
+++ src/components/Elements/DropdownLoggedOut.js
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
 
-export const DropdownLoggedOut = () => {
+export const DropdownLoggedOut = ({setDropdown}) => {
     return (
       <div id="dropdownAvatar" className="select-none	absolute top-10 right-0 z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
           <ul className="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownUserAvatarButton">
               <li>
-                  <Link to="/products" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">All eBooks</Link>
+                  <Link onClick={() => setDropdown(false) } to="/products" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">All eBooks</Link>
               </li>
               <li>
-                  <Link to="/login" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Login</Link>
+                  <Link onClick={() => setDropdown(false) } to="/login" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Login</Link>
               </li>
               <li>
-                  <Link to="/register" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Register</Link>
+                  <Link onClick={() => setDropdown(false) } to="/register" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Register</Link>
               </li>
           </ul>
       </div>
