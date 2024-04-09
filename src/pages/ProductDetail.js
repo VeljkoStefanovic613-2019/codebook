ac06f3d8999d966c135aee13d0963e0e29740ab7
VeljkoStefanovic613-2019 <85968664+VeljkoStefanovic613-2019@users.noreply.github.com>
2024-04-09 15:35:25 +0200

    Out of stock product

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -64,7 +64,7 @@ useEffect(() => {
                   <span className="font-semibold text-blue-500 border bg-slate-100 rounded-lg px-3 py-1 mr-2">{product.size} MB</span>
                 </p>
                 <p className="my-3">
-                  {!inCart && <button onClick={() => addToCart(product)} className={`inline-flex items-center py-2 px-5 text-lg font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800`}>Add To Cart <i className="ml-1 bi bi-plus-lg"></i></button> }
+                  {!inCart && <button onClick={() => addToCart(product)} className={`inline-flex items-center py-2 px-5 text-lg font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 ${product.in_stock ? "" : "cursor-not-allowed"}`} disabled={ product.in_stock ? "" : "disabled" } >Add To Cart <i className="ml-1 bi bi-plus-lg"></i></button> }
                   {inCart && <button onClick={() => removeFromCart(product)} className={`inline-flex items-center py-2 px-5 text-lg font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800`}  disabled={ product.in_stock ? "" : "disabled" }>Remove Item <i className="ml-1 bi bi-trash3"></i></button> }
                 </p>
                 <p className="text-lg text-gray-900 dark:text-slate-200">
